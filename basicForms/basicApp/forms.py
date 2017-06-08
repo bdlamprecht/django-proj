742338a4baab5126a4ee5e9738d0c6764241617e
Brady Lamprecht <bdlamprecht@gmail.com>
2017-06-08 11:54:38 -0600

    Example showing cleaning all fields using builtin validator

--- basicForms/basicApp/forms.py
+++ basicForms/basicApp/forms.py
@@ -1,14 +1,16 @@
 from django import forms
 from django.core import validators
 
-def check_for_z(value):
-    if value[-1].lower() != 'z':
-        raise forms.ValidationError("Name needs to end with the letter 'Z'")
-
 class MyForm(forms.Form):
-    name = forms.CharField(validators=[check_for_z])
+    name = forms.CharField()
     email = forms.EmailField()
+    otheremail = forms.EmailField(label='Enter your email again')
     text = forms.CharField(widget=forms.Textarea)
-    botc = forms.CharField(required=False,
-                          widget=forms.HiddenInput,
-                          validators=[validators.MaxLengthValidator(0)])
+
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vemail = all_clean_data['otheremail']
+
+        if email != vemail:
+            raise forms.ValidationError("Ensure emails match")
