6b38f87b93f927e03dfdcff2d7a91cf601e4ccfb
Brady Lamprecht <bdlamprecht@gmail.com>
2017-06-08 11:30:35 -0600

    Custom botcatcher for django

--- basicForms/basicApp/forms.py
+++ basicForms/basicApp/forms.py
@@ -4,3 +4,11 @@ class MyForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
+    botc = forms.CharField(required=False,
+                          widget=forms.HiddenInput)
+
+    def clean_botc(self):
+        botc = self.cleaned_data['botc']
+        if len(botc) > 0:
+            raise forms.ValidationError("GOTCHA")
+        return botc
