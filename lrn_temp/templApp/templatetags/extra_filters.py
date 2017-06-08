f49594ba1b58cb300ea0da6a41bbf5edf70febcb
Brady Lamprecht <bdlamprecht@gmail.com>
2017-06-08 15:59:41 -0600

    Using `decorators` to register custom filters

--- lrn_temp/templApp/templatetags/extra_filters.py
+++ lrn_temp/templApp/templatetags/extra_filters.py
@@ -2,10 +2,11 @@ from django import template
 
 register = template.Library()
 
-def cut(value,arg):
+@register.filter(name='cut')
+def my_cut(value,arg):
     """
     This cuts out all values of 'arg' from the the string
     """
     return value.replace(arg,'')
 
-register.filter('cut',cut)
+# register.filter('cut',cut)
