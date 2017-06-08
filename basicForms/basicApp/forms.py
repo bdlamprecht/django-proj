d058e5a593a0b9a9db8ffca01c094a7b707b4417
Brady Lamprecht <bdlamprecht@gmail.com>
2017-06-08 11:38:20 -0600

    Working builtin validator for bots

--- basicForms/basicApp/forms.py
+++ basicForms/basicApp/forms.py
@@ -1,14 +1,10 @@
 from django import forms
+from django.core import validators
 
 class MyForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
     botc = forms.CharField(required=False,
-                          widget=forms.HiddenInput)
-
-    def clean_botc(self):
-        botc = self.cleaned_data['botc']
-        if len(botc) > 0:
-            raise forms.ValidationError("GOTCHA")
-        return botc
+                          widget=forms.HiddenInput,
+                          validators=[validators.MaxLengthValidator(0)])
