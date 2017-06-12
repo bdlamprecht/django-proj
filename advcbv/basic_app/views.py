c2e3027209fd7f5afdf59303d0aaf3152594e263
Brady Lamprecht <bdlamprecht@gmail.com>
2017-06-12 09:47:54 -0600

    New "CReate, Update, Delete" views

--- advcbv/basic_app/views.py
+++ advcbv/basic_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import View,TemplateView,ListView,DetailView
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import (View,TemplateView,
+                                  ListView,DetailView,
+                                  CreateView,UpdateView,
+                                  DeleteView)
 from basic_app import models
 
 class IndexView(TemplateView):
@@ -18,3 +22,15 @@ class SchoolDetailView(DetailView):
     context_object_name = 'school_detail'
     model = models.School
     template_name = 'basic_app/school_detail.html'
+
+class SchoolCreateView(CreateView):
+    fields = ('name','princial','location')
+    model = models.School
+
+class SchoolUpdateView(UpdateView):
+    fields = ('name','princial')
+    model = models.School
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
